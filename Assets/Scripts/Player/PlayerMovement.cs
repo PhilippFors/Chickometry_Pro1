2d6f1c985262178385df83396e9095536e9b2d9b
Philipp Forstner <S1810238022@students.fh-hagenberg.at>
2021-10-23 19:10:18 +0200

    simplify character controller

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using Entities.Player.PlayerInput;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -9,26 +10,26 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundMask;
     [SerializeField] private float groundDistance;
+    [SerializeField] private bool isGrounded;
+
+    private Vector2 moveDirection => PlayerInputController.Instance.Movement.ReadValue();
+    private bool jumpTriggered => PlayerInputController.Instance.Jump.Triggered;
     
     private Rigidbody rb;
-    private Vector2 moveDirection;
-    [SerializeField] private bool isGrounded;
-    private float stopTimer = 0.25f;
-    private float stopTimerCount = 0;
-    
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        PlayerInputController.Instance.Jump.Performed += ctx => Jump(ctx);
-        PlayerInputController.Instance.Movement.Performed += ctx => MoveDirection(ctx);
-        PlayerInputController.Instance.Movement.Canceled += ctx => MoveDirection(ctx);
     }
 
-    // Update is called once per frame
+    private void Update()
+    {
+        Jump();
+    }
+
     void FixedUpdate()
     {
         Move();
-        StopMovement();
     }
 
     private void Move()
@@ -43,45 +44,16 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.drag = 0;
         }
-        if (moveDirection != Vector2.zero)
-            stopTimerCount = 0f;
-        
-        Vector3 temp = transform.rotation * new Vector3(moveDirection.x, 0, moveDirection.y);
-        
-        rb.AddForce(temp.normalized * moveSpeed / 0.75f, ForceMode.Acceleration);
 
-        if (isGrounded)
-        {
-            Vector3 xzClamped = new Vector3(rb.velocity.x, 0, rb.velocity.z);
-            xzClamped = Vector3.ClampMagnitude(xzClamped, moveSpeed);
-            rb.velocity = xzClamped + new Vector3(0, rb.velocity.y, 0);
-        }
+        var moveDir = transform.rotation * new Vector3(moveDirection.x, 0, moveDirection.y) * moveSpeed * Time.deltaTime;
+        rb.MovePosition(transform.position + moveDir);
     }
 
-    private void StopMovement()
+    private void Jump()
     {
-        if (moveDirection == Vector2.zero && isGrounded)
-        {
-            stopTimerCount += Time.deltaTime;
-
-            if (stopTimerCount >= stopTimer)
-            {
-                rb.velocity = Vector3.zero;
-            }
-        }
-    }
-
-    private void MoveDirection(InputAction.CallbackContext ctx)
-    {
-        moveDirection = ctx.ReadValue<Vector2>();
-    }
-
-    private void Jump(InputAction.CallbackContext ctx)
-    {
-        if (isGrounded)
+        if (isGrounded && jumpTriggered)
         {
             rb.AddForce(jumpStrength * Vector3.up, ForceMode.VelocityChange);
-            stopTimerCount = 0f;
         }
     }
 }
